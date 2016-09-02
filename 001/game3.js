c8ce9a0c8306e01dbc6ec04d2451ec2ab69ef8f6
Mikołaj Leśny <MakaszML@gmail.com>
2016-09-02 13:52:05 +0200

    change to proper files

--- 001/game3.js
+++ 001/game3.js
@@ -1,4 +1,20 @@
+var amount, currency, PLNc, curTable;
+
 $.get('https://openexchangerates.org/api/latest.json', {app_id: 'b54fc8736f7142118122fddb2c9dc837'}, function(data){
-  for(i in data.rates)
-  $('body').append(i + ':' + data.rates[i] + '</br> ');
+  curTable = JSON.stringify(data);
 });
+
+
+
+$(".currencyIn").keyup(function(){
+  amount = ($(this).val());
+});
+$(".currency").keyup(function(){
+  currency = ($(this).val());
+  console.log(amount + currency);
+});
+
+$(".currencyOut").click(function(){
+  $(this).text((JSON.parse(curTable).rates[currency]*amount).toFixed(2) + " " + currency);
+  alert;
+})
