16645db5c0b5dc681a076388156fcd9481f13c17
Mikołaj Leśny <MakaszML@gmail.com>
2016-09-02 13:39:25 +0200

    removing

--- 005/game3.js
+++
@@ -1,4 +0,0 @@
-$.get('https://openexchangerates.org/api/latest.json', {app_id: 'b54fc8736f7142118122fddb2c9dc837'}, function(data){
-  for(i in data.rates)
-  $('body').append(i + ':' + data.rates[i] + '</br> ');
-});
